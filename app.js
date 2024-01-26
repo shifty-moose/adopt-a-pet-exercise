761227be93ff972cd6727e959dc10c5882a3e710
CiarJ <cjones.de117@gmail.com>
2024-01-26 14:48:28 +0100

    Fixed issue with static links

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
+
+    const animals = Object.keys(petList);
     const htmlRes = `
         <h1>Adopt a Pet</h1>
         <p>Browse through the links below to find your new furry friend:</p>
         <ul>
-            <li><a href="/animals/dogs">Dogs</a></li>
-            <li><a href="/animals/cats">Cats</a></li>
-            <li><a href="/animals/rabbits">Rabbits</a></li>
+            ${animals.map(animal => `
+                <li><a href="/animals/${animal}">${animal.charAt(0).toUpperCase() + animal.slice(1)}</a></li>
+            `).join('')}
         </ul>
     `;
 
